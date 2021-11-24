07e41c2b4deb244d05ac96de9954ac2a2086a24c
VotreProf <94190238+VotreProf@users.noreply.github.com>
2021-11-24 01:08:13 +0100

    Add files via upload

--- js/abonnez.js
+++ js/abonnez.js
@@ -30,4 +30,16 @@ function envoyer() {
   }
  }
 
+
+ document.getElementById('signup').style.display = 'block';
+function salle(){
+    document.getElementById('signup').style.display = 'block';
+    document.getElementById('home-home').style.display = 'none';
+}
+
+function fermer(){
+    document.getElementById('signup').style.display = 'none';
+    document.getElementById('home-home').style.display = 'block';
+}
+
  

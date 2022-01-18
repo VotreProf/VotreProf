e6cbbda90a847e6f85cae81a49a47069e61d52b6
VotreProf <94190238+VotreProf@users.noreply.github.com>
2022-01-17 23:58:15 -0800

    Delete blog directory

--- blog/blog.js
+++
@@ -1,9 +0,0 @@
-const scriptURL = 'https://script.google.com/macros/s/AKfycby_m2aFh6YGPKjKS1BPGhcJo-PcB_tI_bvytrU-Bq0ab8xt0zCrZv4Kc04IQIVLCrmO/exec'
-  const form = document.forms['submit-to-google-sheet']
-
-  form.addEventListener('submit', e => {
-    e.preventDefault()
-    fetch(scriptURL, { method: 'POST', body: new FormData(form)})
-      .then(response => console.log('Success!', response))
-      .catch(error => console.error('Error!', error.message))
-  })

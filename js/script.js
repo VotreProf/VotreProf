b7af502cede12032aeb0c49e6513047c13e553bb
VotreProf <94190238+VotreProf@users.noreply.github.com>
2022-01-19 18:32:44 -0800

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,15 +1,33 @@
-let menu = document.querySelector('#menu-bar');
-let navbar = document.querySelector('.navbar');
+/* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
+function myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+      x.className += " responsive";
+    } else {
+      x.className = "topnav";
+    }
+  }
 
-menu.addEventListener('click', () =>{
-    menu.classList.toggle('fa-times');
-    navbar.classList.toggle('nav-toggle');
-});
+  function downloadApp(){
+    window.location = 'https://drive.google.com/uc?export=download&id=1H-GJUY6_ZpNaCvhlvKPMvE7w1cWppH9K'
+  }
 
-window.onscroll = () =>{
-    menu.classList.remove('fa-times');
-    navbar.classList.remove('nav-toggle');
+
+
+  function sendWa(){
+    var name = document.getElementById("name").value
+    var message = document.getElementById("message").value
+    var produit = 'name';
+    location.href =`https://wa.me/18094513048?&text=Nom%0A ${name}: *Message :*%0A ${message}`;
+
+    window.open(url);
 }
 
-
-
+function openForm() {
+    document.getElementById("myForm").style.display = "block";
+  }
+  
+  function closeForm() {
+    document.getElementById("myForm").style.display = "none";
+    document.getElementById('open-button').style.display = 'block';
+  }
